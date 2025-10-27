54205307d0ff0441d47441a1abdb8f383fb234ee
Rahul Dey <rahuldey9623@gmail.com>
2025-10-27 13:57:41 +0000

    Added postgres host instance with databases

--- Notely.AppHost/Program.cs
+++ Notely.AppHost/Program.cs
@@ -1,7 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Notes_Api>("notes-api");
+var postgres = builder.AddPostgres("postgres")
+    .WithHostPort(5432)
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
 
-builder.AddProject<Projects.Tags_Api>("tags-api");
+var notesDatabase = postgres.AddDatabase("notely-notes");
+var tagsDatabase = postgres.AddDatabase("notely-tags");
+
+var tagsApi = builder.AddProject<Projects.Tags_Api>("tags-api")
+    .WithReference(tagsDatabase)
+    .WaitFor(tagsDatabase);
+
+
+builder.AddProject<Projects.Notes_Api>("notes-api")
+    .WithHttpsEndpoint(5001, name: "public")
+    .WithReference(notesDatabase)
+    .WithReference(tagsApi)
+    .WaitFor(notesDatabase)
+    .WaitFor(tagsApi);
+
 
 builder.Build().Run();
